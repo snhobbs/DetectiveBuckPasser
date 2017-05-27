a3cc71e154c07e2031e63660def7cd29882cd68a
Simon <Simon.Hobbs@electrooptical.net>
2017-05-27 12:22:24 -0400

    added gameEvents

---
+++ gameEvents.py
@@ -0,0 +1,8 @@
+#gameEvents.py
+def checkEvent(charInvent, currRoom):
+	'''
+	Scan through various variables and decide if an event has happened, return true if one has, false if not. Multiple events cannot happen at the same pass through
+	'''
+
+
+	return False
