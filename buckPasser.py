b48561ff5a14660fc706354206a64887ef813204
Simon <Simon.Hobbs@electrooptical.net>
2017-06-12 14:26:07 -0400

    added in a file check for logs and music

--- buckPasser.py
+++ buckPasser.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+import os
 import buckPasserEngine
+
+musicDir = 'music'
+logsAndSaves = "logsAndSaves"
+
+try:
+	os.makedirs(musicDir)
+except FileExistsError:
+	pass
+
+try:
+	os.makedirs(logsAndSaves)
+except FileExistsError:
+	pass
+
 buckPasserEngine.run()
 
