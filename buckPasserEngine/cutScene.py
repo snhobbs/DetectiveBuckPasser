991f00294fb3bee57b46941f0913cf74dd771c6b
Simon <Simon.Hobbs@electrooptical.net>
2017-06-13 10:25:23 -0400

    Added cut scenes

---
+++ buckPasserEngine/cutScene.py
@@ -0,0 +1,28 @@
+from sqlTable import SQLTable
+import userInput
+
+class CutScene(SQLTable):
+	'''
+	When a stage changes, check in the game if a cutscene with the next stage exists and display it
+	'''
+	def __init__(self, db):
+		SQLTable.__init__(self, db)
+		self.table = 'cutScene'
+		self.codeName = 'stage'
+		self.title = self.elementTable.addElement(title = 'Cut scene title', name = 'title', value = None, elementType = 'STRING')
+		self.text = self.elementTable.addElement(title = 'Cut scene main text', name = 'text', value = None, elementType = 'STRING')
+	
+	def play(self):
+		columns, rows = userInput.getTerminalSize()
+		userInput.printToScreen(self.title.value.center(columns), color='cyan')
+		userInput.printToScreen(''.center(columns, '='))
+		textStatements = self.text.value.split('~')
+		for statement in textStatements:
+			lines = userInput.printToScreen(statement)
+			userInput.inputUniversal('...')
+			userInput.clearLines(lines) # clear the input lines
+		userInput.clearLines(1)
+		print("\033[2K", end = '')
+
+if __name__ == "__main__":
+	pass
---
+++ gameFiles/cutScenes.sql
@@ -0,0 +1,3 @@
+insert into cutScene values(1, 'Scene Title', 'Test Cut Scene line 1~Test Cut Scene line 2');
+insert into cutScene values(2, 'Scene Title', 'Test Cut Scene line 1~Test Cut Scene line 2');
+insert into cutScene values(3, 'Scene Title', 'Test Cut Scene line 1~Test Cut Scene line 2');
